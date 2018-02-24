d48866d6e52e58c01061fe8e661e539afdc2fd0c
ubuntu <ubuntu@ubuntu>
2018-02-24 13:01:02 +0500

    task1

---
+++ app.js
@@ -0,0 +1,320 @@
+                                      /* MAROOF ISHAQUE
+                                         ID = 17338
+                                         TTS 2 TO 4
+                                         SIR MUHAMMAD ALI MUGHAL  */
+//-------------------Alert---------------------------
+
+/* Task # 01
+
+alert("Hello");
+
+Task # 02
+alert("Error! Please enter a valid password");
+
+Task # 03
+alert("Welcome to Js  land \n Happy Coding!");
+
+Task # 04
+alert("Hello");
+alert("I m Roy");
+
+Task # 05
+var myName = "Maroof Roy";
+
+Task # 06
+var message = "Hello World";
+alert(message); */
+
+                // Alert End 
+  //------------------------VARIABLES	FOR	STRINGS	---------------------------------
+
+                 // Task # 01
+// var userName = prompt(UserName);
+
+// Task # 02
+// var myName = "Maroof Roy";
+
+// Task # 03
+ // var massage = "Hello World";
+// alert(message);
+
+
+// Task # 04
+// var name = "Maroof Roy";
+// var age = "21 years old";
+// var uni = "KU"
+// var course = "Mobile And Web App Development"
+// alert(name);
+// alert(age);
+// alert(uni);
+// alert(course);
+
+
+// Task # 05
+// var name = "PIZZA";
+// alert("PIZZA"+"\nPIZZ"+"\nPIZ"+"\nPI"+"\nP");
+
+// Task # 06
+// var noOfChildren = "08";
+// var partnersName = "Xyz";
+// var geographicLocation = "KHI";
+// var jobTitle = "Software Developer";
+// alert("You will be a "+ jobTitle + "in "+geographicLocation + ",and married to "+partnersName+"with "+noOfChildren+"kids :)");  
+
+// Task # 07
+// var email = "maroof_roy@yahoo.com";
+// alert("My email address is "+email);
+
+
+// Task # 08
+// var book = "A Smarter way to learn javascript";
+// alert("I m trying to learn from the book "+book);
+
+
+// Task # 09
+// var name = "Maroof Roy";
+// var age = "21 years old";
+// var uni = "KU"
+// var course = "Mobile And Web App Development"
+// alert("My name is "+name+ ",I m "+age+",I study in "+uni+",I take "+course+" Classes ");
+
+
+// Task # 10
+//document.write("Yay! I can write HTML content through Javascript");
+
+
+//          VARIABLES	FOR	STRINGS     END 
+
+//-----------------VARIABLES	FOR	NUMBER-------------------------------------
+
+
+// Task # 01
+// var age = "21";
+// alert("I m " +age + " years old");
+
+// Task # 02
+// var N = "17";
+// alert("You have visited this site " +N + " times");
+
+
+
+// Task # 03
+// var birthYear = 1996;
+// document.write("My birth year is  "+birthYear+"<br> Data type of my Declared variable is Number");
+
+
+// Task # 04
+
+// var name=prompt("your name");
+// var productTitle=prompt("Product \n Shirt \n Pents \n Shoes");
+// var quantity=prompt("Quantity");
+// document.write(name + " Ordered  " + quantity  +productTitle + " on RoyClothing.com");
+
+
+
+//          VARIABLES	FOR	NUMBER     END 
+
+//----------------VariableNames_LegalAndIllegal-------------------------
+
+
+// Task # 01
+//var name,fName,address;
+
+// Task # 02
+// var name,$123,_num,$max_num,_12cell;
+// var 123,@name,#num,nick name;
+
+// Task # 03
+// document.write("<h1>Rules for naming JS variables</h1>");
+// document.write("a.Variable names can only contain Letter,Number,DollarSign and Underscore. For example $my_1stVariable");
+// document.write("<br>b.Variables must begin with a DollarSign, Underscore or Letter. For example $name, _name or name")
+// document.write("<br>d. Variable names are case Sensative.")
+// document.write("<br>e.Variable names should not be JS Keyword.")
+
+//             VariableNames_LegalAndIllegal   END
+
+//-----------------------5-7_MathExpressions----------------------------
+
+// Task # 01
+// var num1 = +prompt("Enter Num1");
+// var num2 = +prompt("Enter Num2");
+// var sum = num1+num2;
+// alert("Sum of "+num1+ " and "+num2+" is " + sum);
+
+
+// Task # 02
+// var num1 = +prompt("Enter Num1");
+// var num2 = +prompt("Enter Num2");
+// var sub = num1-num2;
+// alert("Sub of "+num1+ " and "+num2+" is " + sub);
+// var division = num1/num2;
+// alert("Divide of "+num1+ " and "+num2+" is " + division);
+// var multiply = num1*num2;
+// alert("Multiplication of "+num1+ " and "+num2+" is " + multiply);
+// var reminder = num1%num2;
+// alert("Reminder of "+num1+ " and "+num2+" is " + reminder);
+
+// Task # 03
+// var value =1;
+// document.write("");
+
+// document.write("Value after increment is  " +value );
+
+// value=value+7;
+
+// document.write("<br>value after addition is " +value);
+
+// value--
+
+// document.write("<br>value after decrement is " +value);
+
+
+// Task # 04
+//  var cost="600";
+//  document.write("the cost of 5 ticket is " +cost*5);
+
+// Task # 05
+// var table = prompt("Enter table you want to Display");
+// document.write("<h1>Multiplication Table of  "+table );
+// document.write("<br> </h1><h2>" +table+ "x 1 =" + table*1);
+// document.write("<br> " +table+ "x 2 =" + table*2);
+// document.write("<br> " +table+ "x 3 =" + table*3);
+// document.write("<br> " +table+ "x 4 =" + table*4);
+// document.write("<br> " +table+ "x 5 =" + table*5);
+// document.write("<br> " +table+ "x 6 =" + table*6);
+// document.write("<br> " +table+ "x 7 =" + table*7);
+// document.write("<br> " +table+ "x 8 =" + table*8);
+// document.write("<br> " +table+ "x 9 =" + table*9);
+// document.write("<br> " +table+ "x 10 =" + table*10);
+
+// Task # 06
+
+// var celcius="12";
+// c=((+celcius-32)*5/9);
+// document.write("12'C is"+c);
+
+// var forn="99";
+// f=((+forn*9/5)+32);
+// document.write("<br>99'F is "+f);
+
+
+// Task # 07
+
+// var item1 = +prompt("Price of Item 1");
+// var quantity1 = +prompt("Ordered quantity of item 1");
+// var item2 = +prompt("Price of Item 2 ");
+// var quantity2 = +prompt("Ordered quantity of item 2");
+// var charges = +prompt("Shipping Charges");
+// var total = (item1*quantity1)+(item2*quantity2)+(charges);
+// document.write("Price of Item 1 is " + item1+  "<br> Ordered quantity of item 1 is "+quantity1+" <br> Price of Item 2 is  " +item2 +"<br> Ordered quantity of item 2 is "+quantity2+" <br><br> Shipping Charges  "+charges+ "<br><br>Total cost of your order is "+total);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var x = 2; if (x > 2) {         x = x + 1;  
+//           if (x < 3)                 x = x + 2; 
+//          } else {         x = x + 2;         if (x == 4)                 x = x + 1; } 
+// document.write(x
+
+
+
+// var a = prompt();
+// document.write("number is " + a);
+// var b = Math.round(a);
+// document.write(" <br> round number is " + b);
+// var c = Math.ceil(b);
+// document.write(" <br>ceil number is " + c);
+// var d = Math.floor(c);
+// document.write("<br>floor number is " + d);
+
+
+
+// var a = prompt();
+// b = Math.abs(a);
+// document.write("value is " +b);
+
+
+// var a = Math.random();
+// var b = (a *2)+1;
+// var c = Math.floor(b);
+// document.write("number is " +c);
+
+
+// var a = prompt();
+
+// alert(typeof(a));
+//  var y = toString(a);
+// alert(typeof(y));
+
+// var z = parseInt(y);
+// alert(typeof(z));
+
+
+// var a = Math.random();
+// var b = (a *2);
+// if (b === 1){
+// document.write("Head");
+// }
+// else {
+// document.write("Tail");
+// }
+
+
+
+// var a = 6.955515;
+// var b = a.toFixed();
+// alert(b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
